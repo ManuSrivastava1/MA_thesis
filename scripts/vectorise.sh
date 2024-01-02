c1de99c5c2363c8caeb1caf7b0a6f85588c7682a
Manu Srivastava <92544857+ManuSrivastava1@users.noreply.github.com>
2024-01-02 23:12:50 +0100

    changes file name

--- scripts/vectorise.sh
+++ scripts/vectorise.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bash ./prepare.sh xp_$1
-python3 ../src/vectorise.py --fn_v $1 --model_name $2 --model_path $3
+python3 ../src/vectorise_data.py --fn_v $1 --model_name $2 --model_path $3
